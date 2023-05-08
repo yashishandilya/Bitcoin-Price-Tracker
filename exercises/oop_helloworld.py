109f6f8724b54b248258b83cc5cc53f561676621
Yashi Shandilya <yshand2@uic.edu>
2023-05-08 14:07:20 -0500

    In exercises/oop_helloworld.py, created a class Car with a constructor method and 2 instance methods: getModel, setSpeed

--- exercises/oop_helloworld.py
+++ exercises/oop_helloworld.py
@@ -1,4 +1,17 @@
 # create your class here
+class Car:
+
+    #instance attributes:
+    def __init__(self, model, speed):
+        self.model = model
+        self.speed = speed
+
+    #instance methods
+    def getModel(self):
+        return "{} has speed".format(self.model)
+
+    def setSpeed(self, value):
+        self.speed = value
 
 if __name__ == '__main__':
     bmw = Car('BMW', 30.3)
