7670fa705a53776cb7e2e6b60f2cd77d477f079a
Yashi Shandilya <yshand2@uic.edu>
2023-05-08 15:48:36 -0500

    Completed 4.2 and 4.3 in Training Codelab

--- backend/server.py
+++ backend/server.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=CORS_URLS,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
 
 # TODO (3.1)
--- exercises/sqlite_and_python.py
+++ exercises/sqlite_and_python.py
@@ -5,25 +5,25 @@ TABLE_NAME = 'Bitcoin'
 
 # TODO: Connect to database
 try: # always use a try-catch statement to prevent your programs from crashing in case of a failure
-    
+   db = sqlite3.connect(DATABASE_NAME) 
 except Error as e:
     print(e)
 
 # TODO: get cursor
-
+cursor = db.cursor()
 
 # TODO: define SQL query
-
+sql = "SELECT * FROM '{}';".format(TABLE_NAME)
 
 # TODO: execute sql query
-
+cursor.execute(sql)
 
 # TODO: fetch all results obtained
-
+results = cursor.fetchall()
 
 # TODO: close
-
+cursor.close()
 
 # TODO: print the results
-
+print(results)
 
